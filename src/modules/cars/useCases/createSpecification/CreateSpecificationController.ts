6a9f425109ac528eb78a80b528e88231863504f0
Renan Borges <renanvcb@gmail.com>
2021-10-02 17:11:29 -0300

    controller responses and documentation updated

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -7,13 +7,17 @@ class CreateSpecificationController {
     async handle(request: Request, response: Response): Promise<Response> {
         const { name, description } = request.body;
 
-        const createSpecificationUseCase = container.resolve(
-            CreateSpecificationUseCase
-        );
+        try {
+            const createSpecificationUseCase = container.resolve(
+                CreateSpecificationUseCase
+            );
 
-        await createSpecificationUseCase.execute({ name, description });
+            await createSpecificationUseCase.execute({ name, description });
 
-        return response.status(201).send();
+            return response.status(201).send();
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
     }
 }
 
