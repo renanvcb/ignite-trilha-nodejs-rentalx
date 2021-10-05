8730f790b59bb56762cd4104d430c8bbce89017e
Renan Borges <renanvcb@gmail.com>
2021-10-05 18:06:02 -0300

    hashing user password

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, password, email, driver_license } = request.body;
+
+        const createUserUseCase = container.resolve(CreateUserUseCase);
+
+        try {
+            await createUserUseCase.execute({
+                name,
+                password,
+                email,
+                driver_license,
+            });
+
+            return response.status(201).send();
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    }
+}
+
+export { CreateUserController };
---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+    async execute({
+        name,
+        password,
+        email,
+        driver_license,
+    }: ICreateUserDTO): Promise<void> {
+        const accountAlreadyExists = this.usersRepository.findByEmail(email);
+
+        if (accountAlreadyExists) {
+            throw new Error("Email already in use!");
+        }
+
+        const passwordHash = await hash(password, 8);
+
+        await this.usersRepository.create({
+            name,
+            password: passwordHash,
+            email,
+            driver_license,
+        });
+    }
+}
+
+export { CreateUserUseCase };
