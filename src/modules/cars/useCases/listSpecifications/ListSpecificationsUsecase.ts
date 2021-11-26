f19538aec3b9e99b8cebeed7353d757bebd68def
renanvcb <renanvcb@gmail.com>
2021-11-26 19:18:12 -0300

    refacting imports and adding tsconfig-paths

--- src/modules/cars/useCases/listSpecifications/ListSpecificationsUsecase.ts
+++ src/modules/cars/useCases/listSpecifications/ListSpecificationsUsecase.ts
@@ -1,14 +1,14 @@
 import { inject, injectable } from "tsyringe";
 
-import { Specification } from "../../entities/Specification";
-import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+import { Specification } from "@modules/cars/entities/Specification";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 @injectable()
 class ListSpecificationsUseCase {
   constructor(
     @inject("SpecificationsRepository")
     private specificationsRepository: ISpecificationsRepository
-  ) {}
+  ) { }
   async execute(): Promise<Specification[]> {
     const allSpecifications = await this.specificationsRepository.list();
 
