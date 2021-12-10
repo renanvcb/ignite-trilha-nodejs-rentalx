3ea133a8d700379a2c9228e36c984955210ac175
renanvcb <renanvcb@gmail.com>
2021-12-10 15:21:58 -0300

    added dayjs lib tests almost completed

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
@@ -12,10 +13,44 @@ describe("Create a car rental", () => {
   });
 
   it("Should be able to create a new car rental", async () => {
-    await createRenatalUseCase.execute({
+    const rental = await createRenatalUseCase.execute({
       user_id: "1234",
       car_id: "4321",
       expected_return_date: new Date(),
     });
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("Should not be able to create a new car rental to an user with an active rental", async () => {
+    expect(async () => {
+      await createRenatalUseCase.execute({
+        user_id: "test_user_id",
+        car_id: "any_car_id",
+        expected_return_date: new Date(),
+      });
+
+      await createRenatalUseCase.execute({
+        user_id: "test_user_id",
+        car_id: "any_car_id",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to create a new car rental to a car already rented", async () => {
+    expect(async () => {
+      await createRenatalUseCase.execute({
+        user_id: "any_user_id",
+        car_id: "test_car_id",
+        expected_return_date: new Date(),
+      });
+
+      await createRenatalUseCase.execute({
+        user_id: "any_user_id",
+        car_id: "test_car_id",
+        expected_return_date: new Date(),
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { AppError } from "@shared/errors/AppError";
 
@@ -14,9 +15,7 @@ class CreateRentalUseCase {
     user_id,
     car_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
-    // rent must have a 24 hours minimal duration
-
+  }: IRequest): Promise<Rental> {
     // it shoudn't be possible to create a rent to an user thar has an active car rental
     const userAlreadyRenting =
       await this.rentalsRepository.findOpenRentalByUser(user_id);
@@ -33,6 +32,15 @@ class CreateRentalUseCase {
     if (carUnavailable) {
       throw new AppError("Car is not available");
     }
+
+    // rent must have a 24 hours minimal duration
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
   }
 }
 
